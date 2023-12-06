86514b994274ee2c04d079f9eabb61b466a53e8e
rullo16 <fouo@gft.com>
2023-12-06 15:55:39 +0100

    added statistics for CP

--- CP/src/Solve_CP.py
+++ CP/src/Solve_CP.py
@@ -10,6 +10,7 @@ from typing import List, Tuple, Union
 from utils.logs import print_log, save_solution
 from utils.types import CorrectSolution, SolverMinizinc, ModelType
 from utils.display_results import plot_solution
+from utils.statistics import save_statistics
 
 
 def compute_solution(input_name, model_type: ModelType, solver:SolverMinizinc, timeout:int, free_search, verbose):
@@ -32,6 +33,7 @@ def compute_tests(test_instances, model_type:ModelType, solver:SolverMinizinc, f
 default_input_dir = "..\instances" if os.name == 'nt' else "../instances"
 out_path = "..\out\{model}\{file}" if os.name == 'nt' else "../out/{model}/{file}"
 out_plot_path="..\out\plot\{model}\{file}.png" if os.name == 'nt' else "../out/plot/{model}/{file}.png"
+out_stats_path="..\out\stats\{model}\{file}.csv" if os.name == 'nt' else "../out/stats/{model}/{file}.csv"
 
 def main():
     # Argument parser for command-line options
@@ -39,11 +41,14 @@ def main():
     parser.add_argument("-i", "--in_dir", help="Path to directory containing instances", required=False, type=str)
     parser.add_argument("-o", "--out_dir", help="Path to directory containing the output solutions in .txt format", required=False, type=str)
     parser.add_argument("-r", "--rotation", help="Use rotated circuits", required=False, action='store_true')
+    parser.add_argument("-s", "--solver", help="Solver to use", required=False, type=str, choices=[s.name for s in SolverMinizinc])
     args = parser.parse_args()
 
     # Set input and output directories from arguments or default values
     input_dir = args.in_dir if args.in_dir is not None else default_input_dir
     output_dir = args.out_dir if args.out_dir is not None else out_path
+    solver = SolverMinizinc[args.solver] if args.solver is not None else SolverMinizinc.GECODE
+
 
     # Loop through all input files in the input directory
     for i, input_file in enumerate(glob(os.path.join(input_dir, '*.dzn'))):
@@ -51,17 +56,19 @@ def main():
         input_file = f'ins-{i}.dzn'
         # Call the solver function with the appropriate model file based on the rotation option
         if args.rotation:
-            solution = solve(i+1, ModelType.ROTATION, SolverMinizinc.GECODE, timeout=300, free_search=False)
+            solution = solve(i+1, ModelType.ROTATION, solver, timeout=300, free_search=False)
             print_log(solution)
             if CorrectSolution(solution.status):
-                save_solution(out_path, "rotation", input_file, solution)
+                save_solution(output_dir, "rotation", input_file, solution)
             plot_solution(solution, out_plot_path.format(model="rotation", file=input_file))
+            save_statistics(out_stats_path.format(model="rotation", file=solver), solution)
         else:
-            solution = solve(i+1, ModelType.BASE, SolverMinizinc.GECODE, timeout=300, free_search=False)
+            solution = solve(i+1, ModelType.BASE, solver, timeout=300, free_search=False)
             print_log(solution)
             if CorrectSolution(solution.status):
-                save_solution(out_path, "base", input_file, solution)
+                save_solution(output_dir, "base", input_file, solution)
             plot_solution(solution, out_plot_path.format(model="base", file=input_file))
+            save_statistics(out_stats_path.format(model="base", file=solver), solution)
 if __name__ == '__main__':
     main()
 
--- utils/statistics.py
+++ utils/statistics.py
@@ -5,27 +5,20 @@ import pandas as pd
 from utils.types import CorrectSolution, Solution, StatusEnum
 
 
-def save_statistics(path:str, solution: Solution, configuration=None):
-    columns = ['input_name','status','height', 'solve_time', 'rotation','pos_x','pos_y','configuration']
-    if os.path.exists(path):
-        df = pd.read_csv(path)
+def save_statistics(out_path: str, solution:Solution, config=None):
+    columns=["input_name","status","height","solve_time", "rotation"]
+    if os.path.exists(out_path):
+        df = pd.read_csv(out_path)
     else:
         df = pd.DataFrame(columns=columns)
-    
-    solution.configuration = configuration
 
     solution_vars = vars(solution).copy()
-    solution_vars['status'] = StatusEnum(solution_vars['status']).name
+    solution_vars["status"] = StatusEnum(solution_vars["status"]).name
     if CorrectSolution(solution.status):
-        vals = [solution_vars[c.split("_")[0]][c.split("_")[1]]
-        if c in ["pos_x","pos_y"]
-        else solution_vars[c]
-        for c in columns
-        ]
+        values = [solution_vars[c] for c in columns]
     else:
-        vals = [solution_vars[c] if c in ['input_name','status'] else None for c in columns]
-    
-    vals = [[v] if isinstance(v,list) else v for v in vals]
+        values = [solution_vars[c] if c in ["input_name","status"] else None for c in columns]
+    values = [[v] if isinstance(v, list) else v for v in values]
 
-    df = pd.concat([df, pd.DataFrame(dict(zip(columns,vals)),index=[0])], ignore_index=True)
-    df.to_csv(path, index=False)
+    df = pd.concat([df, pd.DataFrame(dict(zip(columns, values)), index=[0])],ignore_index=True)
+    df.to_csv(out_path, index=False)
